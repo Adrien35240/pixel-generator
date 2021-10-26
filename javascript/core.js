b85dbc275423bb3b73679d74bc96e36a2796942c
Adrien35240 <adriendev35240@gmail.com>
2021-10-26 19:45:47 +0200

    update wheel zoom and delete input zoom

--- javascript/core.js
+++ javascript/core.js
@@ -11,9 +11,6 @@ let app =
             app.grid.appendChild(line);
             for(let rowIndex = 0;rowIndex<pixelX.inputRangeX.value;rowIndex++) {
                 const row = document.createElement('div');
-                line.style.height = zoom.inputZoom.value+'px';
-                row.style.width = zoom.inputZoom.value+'px';
-                row.style.height = zoom.inputZoom.value+'px';
                 row.classList.add('row');
                 row.addEventListener('click',listener.handleRowClick);
                 line.appendChild(row);
@@ -25,7 +22,6 @@ let app =
         app.board.addEventListener('wheel', listener.handleScrollBoard);
         pixelX.inputRangeX.addEventListener('input', pixelX.handlePixelX);
         pixelY.inputRangeY.addEventListener('input', pixelY.handlePixelY);
-        zoom.inputZoom.addEventListener('input', zoom.handleZoom);
         app.drawGrid();
     }
 };
--- javascript/handlers/listener.js
+++ javascript/handlers/listener.js
@@ -1,17 +1,20 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 let listener = {
-    handleScrollBoard: (e) => {
-        e.preventDefault();
-        console.log(e.deltaY); // 53 || -53
-        if (e.deltaY > 0) {
-            zoom.inputZoom.value +=2;
-        }if (e.deltaY < 0) {
-            zoom.inputZoom.value -=2;
-        }
-        app.drawGrid();
+    scale :1,
+    handleScrollBoard: (event) => {
+        event.preventDefault();
+        listener.scale += event.deltaY * -0.01;
+
+        // Restrict scale
+        listener.scale = Math.min(Math.max(.125, listener.scale), 4);
+        console.log('scale',listener.scale);
+        // Apply scale transform
+        app.grid.style.transform = `scale(${listener.scale})`;
     },
-       handleRowClick : (e)=> {
+
+
+    handleRowClick : (e)=> {
         e.target.style.backgroundColor = 'grey';
     },
 };
