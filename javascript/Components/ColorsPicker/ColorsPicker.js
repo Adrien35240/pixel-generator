795f930f0c4cc441919ff978d2f3b78c157258b7
Adrien35240 <adriendev35240@gmail.com>
2021-10-27 12:01:23 +0200

    add transparent color for erase function

--- javascript/Components/ColorsPicker/ColorsPicker.js
+++ javascript/Components/ColorsPicker/ColorsPicker.js
@@ -1,15 +1,19 @@
 colorsPicker = {
-    containerColorsPicker : document.getElementById('color-picker'),
-    colors : ['white','black','red','green'],
-    drawPicker : () => {
-        for(let indexColor = 0;indexColor<colorsPicker.colors.length;indexColor++){
+    containerColorsPicker: document.getElementById('color-picker'),
+    colors: [ 'transparent','white', 'black', 'red', 'green'],
+    drawPicker: () => {
+        for (let indexColor = 0; indexColor < colorsPicker.colors.length; indexColor++) {
             const divColor = document.createElement('div');
             divColor.classList.add('color');
-            divColor.setAttribute('id','color');
+            divColor.setAttribute('id', 'color');
             divColor.style.backgroundColor = colorsPicker.colors[indexColor];
+            console.log('id',divColor.id);
+            if (colorsPicker.colors[indexColor] === 'transparent') {
+                divColor.style.backgroundColor = 'yellow';
+            }
             divColor.dataset.color = colorsPicker.colors[indexColor];
             colorsPicker.containerColorsPicker.appendChild(divColor);
-            divColor.addEventListener('click',handlers.handleClickColor);
+            divColor.addEventListener('click', handlers.handleClickColor);
         }
     }
 };
